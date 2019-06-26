df9011bb6a0321b7732e28200aa3e8eb1d25e4ce
Raj Rathod <raaajrathod@gmail.com>
2019-06-26 22:57:32 +0530

    Clear and Alert Functionality Implemented

--- src/App.js
+++ src/App.js
@@ -1,32 +1,74 @@
 import React, {Component} from "react";
 import Navbar from "./components/layouts/Navbar";
+import Alert from "./components/layouts/Alert";
 import Users from "./components/users/Users";
+import Search from "./components/users/Search";
+import Clear from "./components/users/Clear";
 import axios from "axios";
 import "./App.css";
 
 class App extends Component {
   state = {
     users: [],
-    loading: false
+    loading: false,
+    alert: null
   };
-  async componentDidMount() {
+  // async componentDidMount() {
+  //   this.setState({loading: true});
+  //   const res = await axios.get(
+  //     `https://api.github.com/users?client_id=${
+  //       process.env.REACT_APP_CLIENT_ID
+  //     }&client_secret=${process.env.REACT_APP_CLIENT_SECRET}`
+  //   );
+  //   this.setState({loading: false, users: res.data});
+  //   console.log(res.data);
+  // }
+  // Search Github users
+  searchUsers = async text => {
+    console.log(text);
     this.setState({loading: true});
-    const res = await axios.get(
-      `https://api.github.com/users?client_id=${
-        process.env.REACT_APP_CLIENT_ID
-      }&client_secret=${process.env.REACT_APP_CLIENT_SECRET}`
-    );
-    this.setState({loading: false, users: res.data});
 
-    console.log(res.data);
-  }
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=${
+        process.env.REACT_APP_GITHUB_CLIENT_ID
+      }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    console.log(process.env.REACT_APP_GITHUB_CLIENT_ID);
+    console.log(res);
+    this.setState({loading: false, users: res.data.items});
+  };
+
+  // Clear Users
+
+  clearUsers = () => {
+    this.setState({users: [], loading: false});
+  };
+
+  // show alert
+  showAlert = (msg, type) => {
+    this.setState({
+      alert: {
+        msg: msg,
+        type: type
+      }
+    });
+
+    setTimeout(() => this.setState({alert: null}), 2000);
+  };
 
   render() {
+    const {users, loading, alert} = this.state;
     return (
       <div className='App'>
         <Navbar />
         <div className='container'>
-          <Users loading={this.state.loading} users={this.state.users} />
+          <Alert alert={alert} />
+          <Search searchUsers={this.searchUsers} showAlert={this.showAlert} />
+          <Clear
+            clearUsers={this.clearUsers}
+            showthisBtn={users.length > 0 ? true : false}
+          />
+          <Users loading={loading} users={users} />
         </div>
       </div>
     );
---
+++ src/components/users/Clear.js
@@ -0,0 +1,23 @@
+import React, {Component} from "react";
+
+export class Clear extends Component {
+    render() {
+        const {showthisBtn,clearUsers}=this.props;
+        
+    if (showthisBtn) {
+      return (
+        <div>
+          <button
+            className='btn btn-block btn-light'
+            onClick={clearUsers}>
+            Clear
+          </button>
+        </div>
+      );
+    } else {
+      return "";
+    }
+  } 
+}
+
+export default Clear;
